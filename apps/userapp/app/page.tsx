ca62c147abf803c489e39f9115303bf1ad34a268
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-06-18 23:35:29 +0530

    commit

--- apps/userapp/app/page.tsx
+++ apps/userapp/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
+          <li className="text-2xl">
             Get started by editing <code>apps/web/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>

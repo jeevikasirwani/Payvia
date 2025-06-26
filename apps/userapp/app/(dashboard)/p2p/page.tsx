c8a6d332098f02d7e4974384f24e787d4ae232bb
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-06-26 16:56:02 +0530

    commit final

--- apps/userapp/app/(dashboard)/p2p/page.tsx
+++ apps/userapp/app/(dashboard)/p2p/page.tsx
@@ -1,19 +1,16 @@
-import { Card } from '@repo/ui/card'
-import { TextInput } from '@repo/ui/TextInput'
-
 import React from 'react'
 import SendCard from '../../../components/SendMoney'
 import { JetBrains_Mono } from "next/font/google";
-
 
 const jetbrainsMono = JetBrains_Mono({
   weight: ["400", "600"],
   subsets: ["latin"],
 });
-function page({ title = 'Send Money' }: { title?: string }) {
+
+export default function Page() {
   return (
-   <div className={`${jetbrainsMono.className}  bg-gradient-to-tr from-indigo-200 via-blue-50 to-violet-200 min-h-screen w-full px-4 sm:px-6 lg:px-8 `}>
-        <div className="flex justify-center text-4xl pt-20 font-extrabold">
+    <div className={`${jetbrainsMono.className}  bg-gradient-to-tr from-indigo-200 via-blue-50 to-violet-200 min-h-screen w-full px-4 sm:px-6 lg:px-8 `}>
+      <div className="flex justify-center text-4xl pt-20 font-extrabold">
         <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-800">
           <span className="text-indigo-600">PayVia </span>P2P Transfer
         </h1>
@@ -29,5 +26,3 @@ function page({ title = 'Send Money' }: { title?: string }) {
     </div>
   )
 }
-
-export default page
